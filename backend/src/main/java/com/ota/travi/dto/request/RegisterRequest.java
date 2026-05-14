0ab439263f5d4dc83f8dc0ddab1c2f03561528ad
JohnCB <anhbid1000@gmail.com>
2026-05-14 14:19:24 +0700

    Done backend Sprint 1
    1. Điều chỉnh validation cho register
    2. Cập nhật Readme.md mới nhất cho backend

--- backend/src/main/java/com/ota/travi/dto/request/RegisterRequest.java
+++ backend/src/main/java/com/ota/travi/dto/request/RegisterRequest.java
@@ -1,7 +1,9 @@
-package com.ota.travi.dto;
+package com.ota.travi.dto.request;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 
 public record RegisterRequest(
         @NotBlank(message = "Username không được để trống")
@@ -14,9 +16,14 @@ public record RegisterRequest(
         @NotBlank(message = "Mật khẩu không được để trống")
         String matKhau,
 
-        @NotBlank(message = "Họ tên không được để trống")
+        @NotBlank(message = "Mật khẩu không được để trống")
+        @Size(min = 9 , max = 20 , message = "Mật khẩu phải gồm 8-20 kí tự")
+        @Pattern(regexp = "^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$",
+                message = "Mật khẩu phải chứa ít nhất 1 kí tự in hoa, 1 kí tự thường, 1 chữ số, và 1 kí tự đặc biệt")
         String hoTen,
 
+        @NotBlank(message = "Số điện thoại không được để trống")
+        @Pattern(regexp = "^(\\+84|0)\\d{9}$", message = "Định dạng không hợp lệ")
         String soDienThoai,
 
         // Gửi lên: KHACH_HANG, DOI_TAC
